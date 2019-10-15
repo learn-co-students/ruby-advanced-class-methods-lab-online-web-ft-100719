fa66a3ae2ed9dd13e5b183254f15cd4d78341e68
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-15 19:29:24 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,63 @@ class Song
   def save
     self.class.all << self
   end
+  
+   def self.create
+    song = self.new
+    song.save
+    song
+  end
+   
+   def self.new_by_name(new_song)
+     song = self.new 
+     song.name = new_song
+     song
+   end
+   
+   def self.create_by_name(new_song)
+     song = self.create
+     song.name = new_song
+     song
+   end
+   
+   def self.find_by_name(title)
+     found = self.all.detect {|song| song.name == title}
+     found
+   end
+ 
+  def self.find_or_create_by_name(title)
+       found = self.find_by_name(title)
+    if found
+      found
+    else
+      self.create_by_name(title)
+    end
+  end
+  
+   def self.alphabetical
+    sorted = self.all.sort_by {|song| song.name}
+    sorted
+  end
+  
+  def self.new_from_filename(filename)
+    song_array = filename.split(" - ")
+    song_array[1] = song_array[1].chomp(".mp3")
+    song = self.new
+    song.name = song_array[1]
+    song.artist_name = song_array[0]
+    song
+  end
+  
+  def self.create_from_filename(filename)
+    result = self.new_from_filename(filename)
+    song = self.create
+    song.name = result.name
+    song.artist_name = result.artist_name
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 
 end
