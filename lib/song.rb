922ddf3ad8b0cc6d51896f34b800258af627b676
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-11-13 20:01:32 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,30 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
   def self.all
     @@all
   end
-
   def save
     self.class.all << self
   end
-
+  def self.create
+    song=self.new
+    song.save
+    song
+  end
+  def self.new_by_name(name)
+    song=self.new
+    #binding.pry
+    song.name=name
+    song
+  end
+  def self.create_by_name(name)
+    song=self.new_by_name(name)
+    song.save
+    song
+  end
+  def self.find_by_name(name)
+    self.all.find {|i| if i.name==name }
+  end
 end
